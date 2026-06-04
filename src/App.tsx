ad83440d41eebec9fab0442616dc34aabcbbed7c
Emanuel <emanuelalbaitero@gmail.com>
2026-06-04 15:39:24 -0300

    Version 1.1 Se agregaron los botones de disminuir

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,16 @@ function App() {
   const [count2, setCount2] = useState(0);
   const aumentarCount = () => setCount((count) => count + 1);
   const aumentarCount2 = () => setCount2((count2) => count2 + 1);
-
+  const disminuirCount = () => setCount((count) => count - 1);
+  const disminuirCount2 = () => setCount2((count2) => count2 - 1);
   return (
     <>
       <Title title="Este es el Estado" state={count} />
       <Button label="Aumentar" parentMethod={aumentarCount} />
+      <Button label="Disminuir" parentMethod={disminuirCount} />
       <Title title="Este es otro Estado" state={count2} />
-      <Button label="disminuir" parentMethod={aumentarCount2} />
+      <Button label="Aumentar2" parentMethod={aumentarCount2} />
+      <Button label="Disminuir2" parentMethod={disminuirCount2} />
     </>
   );
 }
