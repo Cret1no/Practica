436cf623dd6ada87dac7df9856c09efb6bec5450
Emanuel <emanuelalbaitero@gmail.com>
2026-06-08 11:54:23 -0300

    Se agrego al segundo boton disminuir la imposibilidad de ser menor a 0

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,11 @@ import { Button, Title } from "./components";
 function App() {
   const [count, setCount] = useState(0);
   const [count2, setCount2] = useState(0);
+
   const aumentarCount = () => setCount((count) => count + 1);
+
   const aumentarCount2 = () => setCount2((count2) => count2 + 1);
+
   const disminuirCount = () => {
     if (count < 0) {
       alert("El estado no puede ser menor a 0");
@@ -15,7 +18,15 @@ function App() {
     setCount(count - 1);
   };
 
-  const disminuirCount2 = () => setCount2((count2) => count2 - 1);
+  const disminuirCount2 = () => {
+    if (count2 < 0) {
+      alert("El estado no puede ser menor a 0");
+      setCount2(0);
+      return;
+    }
+    setCount2(count2 - 1);
+  };
+
   return (
     <>
       <Title title="Este es el Estado" state={count} />
